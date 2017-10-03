00b42ed7bbe68943b08dac0f4a1060c2f5245b19
Omer Kampf <omerkampf@gmail.com>
2017-10-03 16:28:37 +0300

    Nested resources 1

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,7 +16,6 @@ class ReviewsController < ApplicationController
   # GET /reviews/new
   def new
     @review = @restaurant.reviews.build
-    # @review.restaurant_id = params[:restaurant_id]
   end
 
   # GET /reviews/1/edit
@@ -30,11 +29,11 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @restaurant, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        format.html {redirect_to @restaurant, notice: 'Review was successfully created.'}
+        format.json {render :show, status: :created, location: @review}
       else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @review.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -44,11 +43,11 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
+        format.html {redirect_to @review, notice: 'Review was successfully updated.'}
+        format.json {render :show, status: :ok, location: @review}
       else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @review.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -58,23 +57,25 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to reviews_url, notice: 'Review was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
 
+  # Use callbacks to share common setup or constraints between actions.
   def set_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
   end
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = @restaurant.reviews.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def review_params
-      params.require(:review).permit(:reviewer, :rating, :comment, :restaurant_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_review
+    @review = @restaurant.reviews.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def review_params
+    params.require(:review).permit(:reviewer, :rating, :comment, :restaurant_id)
+  end
 end
